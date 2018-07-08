9e74b3673dab67c5841178247aec2277f21d1eb1
Jorge Castro <jorge.castro@outlook.com>
2018-07-07 22:39:10 -0500

    wrong solution

--- PathMobile/PathMobile/Services/PathResolver.cs
+++ PathMobile/PathMobile/Services/PathResolver.cs
@@ -7,56 +7,60 @@ namespace PathMobile.Services
 {
     public class PathResolver : IPathResolver
     {
+        int y = 0;
+        int x = 0;
         public Output Solve(int[,] input)
         {
             var output = new Output();
             int columns = input.GetLength(1);
             int rows = input.GetLength(0);
 
-            for (int y = 0; y < input.GetLength(0); y++)
-            {              
-                for (int x = 0; x < input.GetLength(1); x++)
+            //for (int y = 0; y < input.GetLength(0); y++)
+            //{              
+                for (x = 0; x < columns; x++)
                 {
-                    
-                    if (x == columns)
-                    {
-                        return output;
-                    }
-                    int next = GetNext(input, rows, y, x);
+                    output.Path.Add(y + 1);
+
+                    int next = GetNext(input, columns - 1, rows - 1);
                     output.Cost += next;
-                    output.Path.Add(next);
                     if (output.Cost > 50)
                     {
                         output.Solved = false;
                         return output;
                     }
-
                 }
-            }
+            //}
             return output;
         }
 
-        int GetNext(int[,] matrix, int Xmax, int Y, int X)
+        int GetNext(int[,] matrix, int Xmax, int Ymax)
         {
-            int numUp = matrix[Y == 0 ? Xmax : Y - 1, X + 1];
-            int numMid = matrix[Y, X + 1];
-            int numDown = matrix[Y == Xmax ? 0 : Y + 1, X + 1];
-            return FindLowest(numUp, numMid, numDown);
+            if (x == Xmax) return matrix[y, x];
+
+            int numUp = matrix[y == 0 ? Ymax : y - 1, x + 1];
+            int numMid = matrix[y, x + 1];
+            int numDown = matrix[y == Ymax ? 0 : y + 1, x + 1];
+          
+            int lowest = FindLowestOf3(numUp, numMid, numDown);
+
+            if (y > Ymax) y = 0;
+            if (y < 0) y = Ymax;
+
+            return lowest;
         }
 
-        int FindLowest(int up, int mid, int down)
+        int FindLowestOf3(int up, int mid, int down)
         {
-            if(mid < up)
+            if (mid < up && mid < down)
             {
-                if(mid < down)
-                {
-                    return mid;
-                }
-                else if(down < up)
-                {
-                    return down;
-                }
+                return mid;
             }
+            else if (down < up)
+            {
+                y += 1;
+                return down;
+            }
+            y -= 1;
             return up;
         }
     }
